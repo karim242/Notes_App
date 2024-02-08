9b7d3ed31f8dd94a702bc6a750a86fac446de9af
karim242 <karimelsa38@gmail.com>
2024-02-08 23:05:32 +0200

    add Logic in cubit to save data in Hive

--- lib/cubits/add_notes_cubit.dart
+++ lib/cubits/add_notes_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constant.dart';
+import 'package:notes_app/models/note_model.dart';
 
-
- part 'add_notes_state.dart';
+part 'add_notes_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNotesToBox(NoteModel noteModel) async {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      await notesBox.add(noteModel);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      AddNoteFailure(e.toString());
+    }
+  }
 }
